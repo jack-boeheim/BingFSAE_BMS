7c5ca48e1b37a0e91fd0c0ab0e10d164d2fe56aa
jconnel3 <jconnel3@binghamton.edu>
2024-10-30 21:13:16 +0000

    added functions to check fan speed and operation

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ and its licensor.
 #include "adbms_main.h"
 #include "mbed.h"
 
+
 /*
 https://os.mbed.com/platforms/SDP_K1/
 Mbed SDP-K1 board MOSI, MOSI, SCK definition.
@@ -19,6 +20,8 @@ CSB =  D10
 */
 int main(void);
 void spi_init(void);
+void set_fan_pwm(float PWM_speed, int PWM_period_ns);
+void test_fan(void);
 
 DigitalOut chip_select(D10);                /* SPI chip select */
 DigitalOut mosi(D11);                       /* MOSI */
@@ -28,20 +31,47 @@ SPI spi(D11, D12, D13);                     /* SPI MOSI, MISO, SCK */
 // Serial pc(USBTX, USBRX);                    /* USB Tx,Rx */
 Timer timer;
 
-PwmOut fan(D7);
+PwmOut fan(PE_8); //nucleo f pwm pin 
+DigitalIn fan_status(A1); //stealing this pin for the fan state input
 
 int main()
 {
+    float fan_speed = .50; 
+    int pwm_period_us = 40;
   //  spi_init();
   //  adbms_main();
 
-    fan.write(0.25f);
-    fan.period_us(40);
+    set_fan_pwm(fan_speed, pwm_period_us);
 
     while(1);
 
     return 0;
 }
+
+void set_fan_pwm(float PWM_speed, int PWM_period_ns){
+    fan.write(PWM_speed); //percentage of 100 i.e. .25, .50, etc
+    fan.period_us(PWM_period_ns); //tested at 40 us (25 kHz)
+}
+
+void test_fan(void){
+    float fan_speed = .50; 
+    int pwm_period_us = 40;
+
+    set_fan_pwm(fan_speed, pwm_period_us);
+    wait_us(3000000); //wait 3 seconds for fan to turn everything on and signal to reach board 
+
+    if (fan_status.read() == 1) { //gotta figure out if high is good or bad 
+        printf("bad!\n\n");
+    }
+    else if (fan_status.read() == 0) { //see line 64 
+        printf("good!\n\n");
+    }
+    else{
+        printf("how tf did we get here \n\n");
+    }
+ 
+}
+
 
 void spi_init()
 {   
