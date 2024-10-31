73df420c109de6195e0c99c7215b97a97ab910a2
kahrens1 <kahrens1@binghamton.edu>
2024-10-31 23:41:40 +0000

    Reverted to ADI eval app

--- src/main.cpp
+++ src/main.cpp
@@ -1,84 +1,36 @@
 #include "adbms_main.h"
-#include "main.h"
 
-// Set pin modes
-DigitalIn miso(PIN_SPI_MISO);       // SPI MISO
-DigitalIn driving(PIN_DRIVING);
-DigitalIn charging(PIN_CHARGING);
-
-DigitalOut master(MASTER_ENABLE);
-DigitalOut chip_select(PIN_SPI_CS); // SPI chip select
-DigitalOut mosi(PIN_SPI_MOSI);      // SPI MOSI
-DigitalOut sclk(PIN_SPI_SCLK);      // SPI CLK
-
-// Set SPI pins (MOSI, MISO, SCLK)
-SPI spi(PIN_SPI_MOSI, PIN_SPI_MISO, PIN_SPI_SCLK);
-
-// Configure CAN
-CAN can(PB_8, PB_9);
-
-
-Serial pc(USBTX, USBRX);                    
-
+/*
+https://os.mbed.com/platforms/SDP_K1/
+Mbed SDP-K1 board MOSI, MOSI, SCK definition.
+Arduino Header Pinout
+MOSI = D11
+MISO = D12
+SCK  = D13
+CSB =  D10
+*/
+int main(void);
+void spi_init(void);
+DigitalOut chip_select(D10);                /* SPI chip select */
+DigitalOut mosi(D11);                       /* MOSI */
+DigitalIn miso(D12);                        /* MISO */
+DigitalOut sclk(D13);                       /* SCK */
+SPI spi(D11, D12, D13);                     /* SPI MOSI, MISO, SCK */
+Serial pc(USBTX, USBRX);                    /* USB Tx,Rx */
 Timer timer;
 
-state_t FSM_state = INIT;
 
-float cell_voltages[NUM_PACKS][NUM_CELLS];
-
-
-
-/*-------------------------------------------------------------------------------------------------
- Main Loop
--------------------------------------------------------------------------------------------------*/
-int main() {
-    // Run the AD BMS application
+int main()
+{
+    spi_init();
     adbms_main();
-
-    switch (FSM_state) {
-        case (INIT):
-            spi_init();
-
-            // Send AWAKE message
-            
-            can.write( CANMessage(0x00, "AWAKE", 5, CANData, CANStandard) );
-
-            // Check vehicle is active and charged
-            if ( is_driving() && is_charging() ) {
-                // Write config registers
-                adBms6830_init_config(TOTAL_IC, &IC[0]);
-                // Begin heartbeat
-                
-                // Check current sensor
-
-                // Check battery cell voltage
-                read_cell_voltages(1, &IC[0], );
-
-                // Check the fans
-
-                FSM_state = PRECHARGE;
-            } else {
-                FSM_state = FAULT;
-            }
-            
-            break;
-
-        case (PRECHARGE):
-            break;
-        
-        case (DRIVE_MAIN):
-            break;
-        
-        case (DRIVE_DEBUG):
-            break;
-        
-        case (CHARGE):
-            break;
-        
-        case (FAULT):
-            break;
-    }
-    
     return 0;
 }
 
+void spi_init()
+{   
+    chip_select = 1;
+    spi.format(8,0);            /* 8bit data, CPOL-CPHA= 0*/
+    spi.frequency(500000);     /* SPI clock 2Mhz */
+    pc.baud(115200);              /* Usb baud rate */   
+}
