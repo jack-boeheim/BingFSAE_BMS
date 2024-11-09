ccbbf8a2050acb99762572c526840c26a5060990
kahrens1 <kahrens1@binghamton.edu>
2024-11-09 23:16:10 +0000

    Changed can.write position

--- src/main.cpp
+++ src/main.cpp
@@ -36,92 +36,98 @@ cell_asic IC[NUM_MODULES];
  Main Loop
 -------------------------------------------------------------------------------------------------*/
 int main() {
-    
+
+
+    can.frequency(CAN_BAUD_RATE_DRIVE);
     uint16_t test_IV = 0xFFFF;
     uint16_t test_res = 0xF0F0;
     uint16_t test_OCV = 0x0F0F;
 
-    voltage_can_message(test_IV, test_res, test_OCV);
+ 
+ while(1){
+ 
+     voltage_can_message(test_IV, test_res, test_OCV);
+ 
+ }
 
+    // switch (FSM_state) {
+    //     case (INIT):
+    //         fault = 1;
+    //         Delay_ms(1);
+    //         spi_init(); 
 
-    switch (FSM_state) {
-        case (INIT):
-            fault = 1;
-            Delay_ms(1);
-            spi_init(); 
+    //         // Send AWAKE message
+    //       //   can.write(CANMessage(0x00, "AWAKE", 5, CANData, CANStandard) );
 
-            // Send AWAKE message
-          //   can.write(CANMessage(0x00, "AWAKE", 5, CANData, CANStandard) );
+    //         // Check if Accumulator is in drive mode
+    //         if (is_driving() && !is_charging()) {
+    //             // Write config registers
+    //             adBms6830_init_config(TOTAL_IC, &IC[0]);
 
-            // Check if Accumulator is in drive mode
-            if (is_driving() && !is_charging()) {
-                // Write config registers
-                adBms6830_init_config(TOTAL_IC, &IC[0]);
+    //             // Check current sensor (use same method as ECU code to cook value)
+    //             // Check battery cell voltage (run reading and check OV/UV flag & others)
+    //             // read_cell_voltages(1, &IC[0], );
+    //             // Check the fans (Joe to add fan check function)
+    //              // Will need to check all Flgas before doing this (maybe create a function to do so)
+    //             // assert_fault_low();
+    //             FSM_state = PRECHARGE;
+    //         } 
+    //         else if(is_charging() && !is_driving()) {
+    //             FSM_state = CHARGE;
+    //         }
 
-                // Check current sensor (use same method as ECU code to cook value)
-                // Check battery cell voltage (run reading and check OV/UV flag & others)
-                // read_cell_voltages(1, &IC[0], );
-                // Check the fans (Joe to add fan check function)
-                 // Will need to check all Flgas before doing this (maybe create a function to do so)
-                // assert_fault_low();
-                FSM_state = PRECHARGE;
-            } 
-            else if(is_charging() && !is_driving()) {
-                FSM_state = CHARGE;
-            }
-
-            else {
-                FSM_state = FAULT;
-            }
+    //         else {
+    //             FSM_state = FAULT;
+    //         }
             
-            break;
+    //         break;
 
-        case (PRECHARGE):
-            fault = 0;
-                while(!is_shutdown_closed()){
-                    //Wait until SDC is closed
-                }
-            Delay_ms(500); //Wait for Precharge (500ms after SDC closed) (temporary should accept input from Tractive Precharge control board)    
-            FSM_state = DRIVE_MAIN;
-            break;
+    //     case (PRECHARGE):
+    //         fault = 0;
+    //             while(!is_shutdown_closed()){
+    //                 //Wait until SDC is closed
+    //             }
+    //         Delay_ms(500); //Wait for Precharge (500ms after SDC closed) (temporary should accept input from Tractive Precharge control board)    
+    //         FSM_state = DRIVE_MAIN;
+    //         break;
         
-        case (DRIVE_MAIN): 
+    //     case (DRIVE_MAIN): 
           
-          can.frequency(CAN_BAUD_RATE_DRIVE);
-          adBms6830_write_config(TOTAL_IC, &IC[0]); 
-          adBms6830_start_adc_cell_voltage_measurment(TOTAL_IC);
-          adBms6830_start_adc_s_voltage_measurment(TOTAL_IC);
-          adBms6830_start_aux_voltage_measurment(TOTAL_IC, &IC[0]);
+    //       can.frequency(CAN_BAUD_RATE_DRIVE);
+    //       adBms6830_write_config(TOTAL_IC, &IC[0]); 
+    //       adBms6830_start_adc_cell_voltage_measurment(TOTAL_IC);
+    //       adBms6830_start_adc_s_voltage_measurment(TOTAL_IC);
+    //       adBms6830_start_aux_voltage_measurment(TOTAL_IC, &IC[0]);
 
-            while(is_shutdown_closed()){
-               measurement_loop();
-               //voltage_can_message(&IC[0]);
-               Delay_ms(10);
-            }
+    //         while(is_shutdown_closed()){
+    //            measurement_loop();
+    //            //voltage_can_message(&IC[0]);
+    //            Delay_ms(10);
+    //         }
             
-            FSM_state = FAULT;
+    //         FSM_state = FAULT;
             
-            break;
+    //         break;
         
-        case (DRIVE_DEBUG):
-         //Need to figure out a good way to switch into this state (code macro, or input pin)
-            can.frequency(CAN_BAUD_RATE_DRIVE);
-            break;
+    //     case (DRIVE_DEBUG):
+    //      //Need to figure out a good way to switch into this state (code macro, or input pin)
+    //         can.frequency(CAN_BAUD_RATE_DRIVE);
+    //         break;
         
-        case (CHARGE):
+    //     case (CHARGE):
             
-             can.frequency(CAN_BAUD_RATE_CHARGE);
-             measurement_loop();
-             //voltage_can_message(&IC[0]);
-            break;
+    //          can.frequency(CAN_BAUD_RATE_CHARGE);
+    //          measurement_loop();
+    //          //voltage_can_message(&IC[0]);
+    //         break;
         
-        case (FAULT):
-           // bms_fault = 1;  
-          //  assert_fault_high();
-          // Include CAN message showing Fault Code 
-            break;
-    }
-    
+    //     case (FAULT):
+    //        // bms_fault = 1;  
+    //       //  assert_fault_high();
+    //       // Include CAN message showing Fault Code 
+    //         break;
+    // }
+
     return 0;
 }
 
