9ee2fc0ef54b1c14a179fd4f85969dcddc61cd53
kahrens1 <kahrens1@binghamton.edu>
2025-01-15 15:42:16 -0500

    updated to most recent, check against project submission

--- matlab/EECE580B_Final_Project.m
+++ matlab/EECE580B_Final_Project.m
@@ -8,15 +8,22 @@ SOC = SOC/100;
 R_0 = 32e-3; %Internal Resistance Per Manufacturer
 OCV = OCV + R_0*3; %Voltage/SOC curve provided gives terminal voltages at 1C. Approximate OCV = V_t + R_0*I
 load("discharge_data.mat");
-dt = zeros(size(data,1),1);
 
 OCV_SOC = fit(SOC,OCV,'poly3'); %Fit Manufacture Provided Data to 3rd Order Poly
+OCV_SOC_fit = OCV_SOC.p1*SOC.^3 + OCV_SOC.p2*SOC.^2 ...
+        + OCV_SOC.p3.*SOC + OCV_SOC.p4;
+subplot(2,1,1);
 plot(SOC,OCV_SOC.p1*SOC.^3 + OCV_SOC.p2*SOC.^2 ...
         + OCV_SOC.p3.*SOC + OCV_SOC.p4);
-hold on; 
-plot(SOC,OCV,'o');
 xlabel('SOC (%)');
 ylabel("Open Circuit Voltage (V)");
+
+hold on;
+plot(SOC,OCV,'o');
+subplot(2,1,2);
+plot(SOC, abs(OCV - OCV_SOC_fit));
+
+
 
 R_1 = 1.6e-3; %Diffusion and Charge Transfer Parameters/Per Literature as Placeholder for now
 tau_1 = 3.68;
